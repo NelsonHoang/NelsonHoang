278534da8aa93e9f983b105ef8a36caa8472688a
NelsonH <nelson.hoang@gmail.com>
2024-03-28 09:45:45 -0700

    [MainSection] Add subtexts

--- site/src/jsMain/kotlin/org/nelsonhoang/com/sections/MainSection.kt
+++ site/src/jsMain/kotlin/org/nelsonhoang/com/sections/MainSection.kt
@@ -106,5 +106,27 @@ fun MainText(breakpoint: Breakpoint) {
         ) {
             Text("Hello, I am")
         }
+        P(
+            attrs = Modifier
+                .margin(top = 20.px, bottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(if (breakpoint >= Breakpoint.LG) 68.px else 40.px)
+                .fontWeight(FontWeight.Bolder)
+                .color(Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text("Nelson Hoang")
+        }
+        P(
+            attrs = Modifier
+                .margin(top = 10.px, bottom = 5.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(30.px)
+                .fontWeight(FontWeight.Bold)
+                .color(Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text("Android Developer with Backend Experience")
+        }
     }
 }
