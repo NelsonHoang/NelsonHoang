0666fd1b83302d8ddc2e39af3862d397da8e0897
NelsonH <nelson.hoang@gmail.com>
2024-03-28 23:49:55 -0700

    [MainText] Add sample body text and extract header texts into constants

--- site/src/jsMain/kotlin/org/nelsonhoang/com/components/MainText.kt
+++ site/src/jsMain/kotlin/org/nelsonhoang/com/components/MainText.kt
@@ -11,46 +11,65 @@ import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 import org.nelsonhoang.com.model.Theme
-import org.nelsonhoang.com.util.Constants
+import org.nelsonhoang.com.util.Constants.FONT_FAMILY
+
+const val HEADER_TEXT1 = "Hello, I am"
+const val HEADER_TEXT2 = "Nelson Hoang"
+const val HEADER_TEXT3 = "Android Developer with Backend Experience"
+const val BODY_TEXT =
+    "I've worked for Amazon Music, Skulpt.me and the company formerly known as Facebook"
 
 /**
  * Component for all the text shown in the [MainSection]
  */
 @Composable
 fun MainText(breakpoint: Breakpoint) {
-    Column {
+    Column{
         P(
             attrs = Modifier
                 .margin(topBottom = 0.px)
-                .fontFamily(Constants.FONT_FAMILY)
+                .fontFamily(FONT_FAMILY)
                 .fontSize(if (breakpoint >= Breakpoint.LG) 45.px else 20.px)
                 .fontWeight(FontWeight.Normal)
                 .color(Theme.Primary.rgb)
                 .toAttrs()
         ) {
-            Text("Hello, I am")
+            Text(HEADER_TEXT1)
         }
         P(
             attrs = Modifier
                 .margin(top = 20.px, bottom = 0.px)
-                .fontFamily(Constants.FONT_FAMILY)
+                .fontFamily(FONT_FAMILY)
                 .fontSize(if (breakpoint >= Breakpoint.LG) 68.px else 40.px)
                 .fontWeight(FontWeight.Bolder)
                 .color(Theme.Secondary.rgb)
                 .toAttrs()
         ) {
-            Text("Nelson Hoang")
+            Text(HEADER_TEXT2)
         }
         P(
             attrs = Modifier
                 .margin(top = 10.px, bottom = 5.px)
-                .fontFamily(Constants.FONT_FAMILY)
+                .fontFamily(FONT_FAMILY)
                 .fontSize(30.px)
                 .fontWeight(FontWeight.Bold)
                 .color(Theme.Secondary.rgb)
                 .toAttrs()
         ) {
-            Text("Android Developer with Backend Experience")
+            Text(HEADER_TEXT3)
+        }
+        P(
+            attrs = Modifier
+                .margin(bottom = 25.px)
+                .maxWidth(400.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(35.px)
+                .fontWeight(FontWeight.Normal)
+                .color(Theme.Secondary.rgb)
+                .lineHeight(48.px)
+                .toAttrs()
+        ) {
+            Text(BODY_TEXT)
         }
     }
 }
--- site/src/jsMain/kotlin/org/nelsonhoang/com/sections/MainSection.kt
+++ site/src/jsMain/kotlin/org/nelsonhoang/com/sections/MainSection.kt
@@ -1,33 +1,25 @@
 package org.nelsonhoang.com.sections
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.ObjectFit
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.layout.SimpleGrid
 import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
-import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.dom.P
-import org.jetbrains.compose.web.dom.Text
 import org.nelsonhoang.com.components.Header
 import org.nelsonhoang.com.components.MainText
 import org.nelsonhoang.com.model.Section
-import org.nelsonhoang.com.model.Theme
 import org.nelsonhoang.com.style.MainImageStyle
-import org.nelsonhoang.com.util.Constants.FONT_FAMILY
 import org.nelsonhoang.com.util.Constants.SECTION_WIDTH
 import org.nelsonhoang.com.util.Res
 
