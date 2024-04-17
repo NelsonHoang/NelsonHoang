e27f5813ab6ad4bcf9cb05788d122f00e47b9557
NelsonH <nelson.hoang@gmail.com>
2024-04-17 11:37:10 -0700

    [SectionTitle] Add animation and refactor code for readability

--- site/src/jsMain/kotlin/org/nelsonhoang/com/components/SectionTitle.kt
+++ site/src/jsMain/kotlin/org/nelsonhoang/com/components/SectionTitle.kt
@@ -10,6 +10,10 @@ import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.web.css.CSSSizeValue
+import org.jetbrains.compose.web.css.CSSUnit
 import org.jetbrains.compose.web.css.ms
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
@@ -17,9 +21,10 @@ import org.jetbrains.compose.web.dom.Text
 import org.nelsonhoang.com.model.Section
 import org.nelsonhoang.com.model.Theme
 import org.nelsonhoang.com.util.Constants.FONT_FAMILY
+import org.nelsonhoang.com.util.ObserveViewPortEntered
 
 /**
- * Used to label each Section of the Home Page based on their title and subtitle
+ * Used to label each Section of the Home Page based on their title and subtitle.
  */
 @Composable
 fun SectionTitle(
@@ -27,78 +32,129 @@ fun SectionTitle(
     section: Section,
     alignment: Alignment.Horizontal = Alignment.Start
 ) {
-    val titleMargin by remember { mutableStateOf(50.px) }
-    val subtitleMargin by remember { mutableStateOf(50.px) }
+    val scope = rememberCoroutineScope()
+    var titleMargin by remember { mutableStateOf(50.px) }
+    var subtitleMargin by remember { mutableStateOf(50.px) }
+
+    ObserveViewPortEntered(
+        sectionId = section.id,
+        distanceFromTop = 700.0,
+        onViewportEntered = {
+            scope.launch {
+                subtitleMargin = 0.px
+                if (alignment == Alignment.Start) {
+                    delay(25)
+                }
+                titleMargin = 0.px
+            }
+        }
+    )
 
     Column(
         modifier = modifier,
         horizontalAlignment = alignment
     ) {
-        P(
-            attrs = Modifier
-                .fillMaxWidth()
-                .textAlign(
-                    when (alignment) {
-                        Alignment.CenterHorizontally -> TextAlign.Center
-                        Alignment.End -> TextAlign.End
-                        else -> TextAlign.Start
-                    }
-                )
-                .margin(
-                    left = titleMargin,
-                    top = 0.px,
-                    bottom = 0.px
-                )
-                .fontFamily(FONT_FAMILY)
-                .fontSize(25.px)
-                .fontWeight(FontWeight.Normal)
-                .color(Theme.Primary.rgb)
-                .transition(
-                    CSSTransition(
-                        property = "margin",
-                        duration = 300.ms
-                    )
-                )
-                .toAttrs()
-        ) {
-            Text(section.title)
-        }
-        P(
-            attrs = Modifier
-                .fillMaxWidth()
-                .textAlign(
-                    when (alignment) {
-                        Alignment.CenterHorizontally -> TextAlign.Center
-                        Alignment.End -> TextAlign.End
-                        else -> TextAlign.Start
-                    }
-                )
-                .margin(
-                    left = if (alignment == Alignment.Start) subtitleMargin else 0.px,
-                    right = if (alignment == Alignment.CenterHorizontally) subtitleMargin else 0.px,
-                    bottom = 10.px,
-                    top = 0.px
-                )
-                .fontFamily(FONT_FAMILY)
-                .fontSize(40.px)
-                .fontWeight(FontWeight.Bold)
-                .color(Theme.Secondary.rgb)
-                .transition(
-                    CSSTransition(
-                        property = "margin",
-                        duration = 300.ms
-                    )
-                )
-                .toAttrs()
-        ) {
-            Text(section.subtitle)
-        }
-        Box(
-            modifier = Modifier
-                .height(2.px)
-                .width(80.px)
-                .backgroundColor(Theme.Primary.rgb)
-                .borderRadius(r = 50.px)
-        )
+        createHeaderText(section, alignment, titleMargin)
+        createSubtitleText(section, alignment, subtitleMargin)
+        createDividerLine()
     }
+}
+
+/**
+ * @return [P] representing the title text of the section title.
+ */
+@Composable
+private fun createHeaderText(
+    section: Section,
+    alignment: Alignment.Horizontal,
+    titleMargin: CSSSizeValue<CSSUnit.px>
+) {
+    return P(
+        attrs = Modifier
+            .fillMaxWidth()
+            .textAlign(
+                when (alignment) {
+                    Alignment.CenterHorizontally -> TextAlign.Center
+                    Alignment.End -> TextAlign.End
+                    else -> TextAlign.Start
+                }
+            )
+            .margin(
+                left = titleMargin,
+                top = 0.px,
+                bottom = 0.px
+            )
+            .fontFamily(FONT_FAMILY)
+            .fontSize(25.px)
+            .fontWeight(FontWeight.Normal)
+            .color(Theme.Primary.rgb)
+            .transition(
+                createSectionTitleTransition()
+            )
+            .toAttrs()
+    ) {
+        Text(section.title)
+    }
+}
+
+/**
+ * @return [P] representing the subtitle text of the section title.
+ */
+@Composable
+private fun createSubtitleText(
+    section: Section,
+    alignment: Alignment.Horizontal,
+    subtitleMargin: CSSSizeValue<CSSUnit.px>
+) {
+    return P(
+        attrs = Modifier
+            .fillMaxWidth()
+            .textAlign(
+                when (alignment) {
+                    Alignment.CenterHorizontally -> TextAlign.Center
+                    Alignment.End -> TextAlign.End
+                    else -> TextAlign.Start
+                }
+            )
+            .margin(
+                left = if (alignment == Alignment.Start) subtitleMargin else 0.px,
+                right = if (alignment == Alignment.CenterHorizontally) subtitleMargin else 0.px,
+                bottom = 10.px,
+                top = 0.px
+            )
+            .fontFamily(FONT_FAMILY)
+            .fontSize(40.px)
+            .fontWeight(FontWeight.Bold)
+            .color(Theme.Secondary.rgb)
+            .transition(
+                createSectionTitleTransition()
+            )
+            .toAttrs()
+    ) {
+        Text(section.subtitle)
+    }
+}
+
+/**
+ * @return [Box] represented as a lien divider below the title and subtitle.
+ */
+@Composable
+private fun createDividerLine() {
+    Box(
+        modifier = Modifier
+            .height(2.px)
+            .width(80.px)
+            .backgroundColor(Theme.Primary.rgb)
+            .borderRadius(r = 50.px)
+    )
+}
+
+/**
+ * @return [CSSTransition] that animates the text elements forward.
+ */
+private fun createSectionTitleTransition(): CSSTransition {
+    return CSSTransition(
+        property = "margin",
+        duration = 300.ms
+    )
 }
