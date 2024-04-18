df2f169ddd345f2fa5e6440e11fc35bae2122f8d
NelsonH <nelson.hoang@gmail.com>
2024-04-17 17:29:02 -0700

    [ExperienceSection] Updating description for Amazon and Skulpt

--- site/src/jsMain/kotlin/org/nelsonhoang/com/model/Experience.kt
+++ site/src/jsMain/kotlin/org/nelsonhoang/com/model/Experience.kt
@@ -22,7 +22,16 @@ enum class Experience(
     Second(
         number = "02",
         jobPosition = "Lead Android Developer / Full Stack Engineer",
-        description = "Built Skulpt's first Android app along with helping out with the Ruby on Rails backend",
+        description = "I was itching to experience a start-up company and joined" +
+                "Skulpt as their first Android engineer. Skulpt created a " +
+                "device, a little smaller than modern iPhones, to use electrical" +
+                "impedance in order to to detect body fat and muscle " +
+                "hypertrophy. I built v1 of the Android and led two other " +
+                "engineers to implement the app's redesign. When needed I'd " +
+                "pick up Ruby on Rails backend work. We were found in Best Buy " +
+                "and sold 30,000 units. Ultimately, we failed to sell at a scale " +
+                "needed like FitBit. Nonetheless, I cherish taking a product " +
+                "from 0 to 1.",
         company = "Skulpt",
         from = "September 2014",
         to = "April 2017",
@@ -30,7 +39,12 @@ enum class Experience(
     Third(
         number = "01",
         jobPosition = "Android Developer",
-        description = "Worked on the Amazon Music app for Prime Music and the Kindle Fire",
+        description = "I was part of Amazon Music in San Francisco, and we were " +
+                "tasked with launching Prime Music on the Kindle Fire and " +
+                "Android devices. It was an exciting time to be in S.F.as it was" +
+                "my first job after college and unicorn companies were being " +
+                "formed seemingly overnight. This is where I was blessed to meet " +
+                "my mentors, who help me navigate life as a software engineer.",
         company = "Amazon",
         from = "August 2014",
         to = "July 2012",
