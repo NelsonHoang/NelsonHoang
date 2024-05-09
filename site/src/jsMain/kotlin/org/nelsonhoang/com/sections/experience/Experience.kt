a5e37f1cd0ce3ab2f5abc0b32a84599a346fb16a
NelsonH <nelson.hoang@gmail.com>
2024-05-08 21:10:24 -0700

    [ExperienceSection] Fix Text

--- site/src/jsMain/kotlin/org/nelsonhoang/com/sections/experience/Experience.kt
+++ site/src/jsMain/kotlin/org/nelsonhoang/com/sections/experience/Experience.kt
@@ -15,9 +15,9 @@ enum class Experience(
         number = "03",
         jobPosition = "Senior Android Developer",
         description = "Facebook was my top choice while I was recruiting in 2017" +
-                "for two main reasons. Reason number one was to experience the " +
+                " for two main reasons. Reason number one was to experience the " +
                 "Facebook culture I heard so much about of \"move fast and " +
-                "break things.\" The second reason was to use the basketball" +
+                "break things.\" The second reason was to use the basketball " +
                 "court, where I am happy to say I have met many friends \uD83D\uDE03 " +
                 "I felt like I grew the most as a Software Engineering during my " +
                 "time at the company formerly known as Facebook. I got to build" +
@@ -31,10 +31,10 @@ enum class Experience(
     Second(
         number = "02",
         jobPosition = "Lead Android Developer / Full Stack Engineer",
-        description = "I was itching to experience a start-up company and joined" +
+        description = "I was itching to experience a start-up company and joined " +
                 "Skulpt as their first Android engineer. Skulpt created a " +
                 "device, a little smaller than modern iPhones, to use electrical" +
-                "impedance in order to to detect body fat and muscle " +
+                " impedance in order to to detect body fat and muscle " +
                 "hypertrophy. I built v1 of the Android and led two other " +
                 "engineers to implement the app's redesign. When needed I'd " +
                 "pick up Ruby on Rails backend work. We were found in Best Buy " +
@@ -50,7 +50,7 @@ enum class Experience(
         jobPosition = "Android Developer",
         description = "I was part of Amazon Music in San Francisco, and we were " +
                 "tasked with launching Prime Music on the Kindle Fire and " +
-                "Android devices. It was an exciting time to be in S.F.as it was " +
+                "Android devices. It was an exciting time to be in S.F. as it was " +
                 "my first job after college and unicorn companies were being " +
                 "formed seemingly overnight. This is where I was blessed to meet " +
                 "my mentors, who help me navigate life as a software engineer.",
